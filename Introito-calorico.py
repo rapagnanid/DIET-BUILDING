5f6b2f59458e12ce65975e15ee81ebfe54b0c7df
Davide Rapagnani <d.rapagnani@reply.it>
2022-11-17 18:00:49 +0100

    Create class CalorieIntake

---
+++ Introito-calorico.py
@@ -0,0 +1,12 @@
+class CalorieIntake:
+    """
+    Creates a calorie intake class.
+    """
+
+    def __init__(self, name, period):
+        self.name = name
+        self.period = period
+
+    def calculator(self, food_name, food_amount):
+        pass
+
