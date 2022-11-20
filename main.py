fabc1c829d1647c8fb4b982b956386e3ec87cdab
Davide Rapagnani <d.rapagnani@reply.it>
2022-11-20 13:16:20 +0100

    Fix the food input stop exit and go to calculator in main.py

--- main.py
+++ main.py
@@ -6,26 +6,27 @@ check_food_list = df['Alimenti'].tolist()
 #food_name = []
 #food_amount = []
 
-i = 'STOP'
+stop = 'STOP'
+fatto = 'FATTO'
 
 name = input("Ciao, come ti chiami?: ")
 while True:
-    user_input = input("Per favore, inserisci un alimento, oppure digita STOP per uscire: ")
-    if user_input == i:
+    user_input = input("Per favore, inserisci un alimento: ")
+    filtered_food_list = filter(lambda x: x.startswith(user_input), check_food_list)
+    if len(str(filtered_food_list)) > 1:
+        print('Ci sono una o più opzioni disponibili per questo alimento:'.format(user_input))
+        for index, name in enumerate(filtered_food_list):
+            print("{0}".format(name))
+    food_name = input('Per favore, inserisci nuovamente un alimento, scegliendo tra le possibili opzioni: ')
+    food_amount = float(input('Per favore, inserisci la quantità di tale alimento (g): '))
+    #inputed_food_list = [].append(food_name, food_amount)
+    next_action = input('Per favore, premi ENTER per continuare, altrimenti digita FATTO per andare al calcolo oppure STOP terminare: ')
+    if next_action == fatto:
+        calorie_intake = DailyCalorieIntake(name)
+        print(f"{calorie_intake.name} assumerà giornalmente: ")
+        print(calorie_intake.calculator(food_name, food_amount))
         break
-    else:
-        filtered_food_list = filter(lambda x: x.startswith(user_input), check_food_list)
-        if len(str(filtered_food_list)) > 1:
-            print('Ci sono una o più opzioni disponibili per questo alimento:'.format(user_input))
-            for index, name in enumerate(filtered_food_list):
-                print("{0}".format(name))
-        food_name = input('Per favore, inserisci nuovamente un alimento, scegliendo tra le possibili opzioni, oppure digita STOP per uscire: ')
-        if food_name == i:
-            break
-        else:
-            food_amount = float(input("Qual è la quantità di tale alimento? E.g. (g): "))
+    elif next_action == stop:
+        break
 
-            calorie_intake = DailyCalorieIntake(name)
 
-            print(f"{calorie_intake.name} assumerà: ")
-            print(calorie_intake.calculator(food_name, food_amount))
