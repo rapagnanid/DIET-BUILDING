05cc9c64f912651cec950d01aa980ef565d250fc
Davide Rapagnani <d.rapagnani@reply.it>
2022-11-23 18:45:16 +0100

    fix all the issues in introitocalorico.py

--- introitocalorico.py
+++ introitocalorico.py
@@ -14,6 +14,11 @@ class DailyCalorieIntake:
     pro_count = []
     lip_count = []
 
+    calorie = 0
+    carboidrati = 0
+    proteine = 0
+    lipidi = 0
+
     def __init__(self, user_name, food_list_name, food_list_amount):
         self.user_name = user_name
         self.food_list_name = food_list_name
@@ -22,24 +27,29 @@ class DailyCalorieIntake:
     def calculator(self):
 
         for i in range(0, len(self.food_list_name)):
+            self.cal_count.append(int((df_def.loc[df_def['Alimenti'] == self.food_list_name[i], 'KCal'].iloc[0] * self.food_list_amount[i] / 100).round(0)))
+            self.carb_count.append(int((df_def.loc[df_def['Alimenti'] == self.food_list_name[i], 'Carboidrati (g)'].iloc[0] * self.food_list_amount[i] / 100).round(0)))
+            self.pro_count.append(int((df_def.loc[df_def['Alimenti'] == self.food_list_name[i], 'Proteine (g)'].iloc[0] * self.food_list_amount[i] / 100).round(0)))
+            self.lip_count.append(int((df_def.loc[df_def['Alimenti'] == self.food_list_name[i], 'Lipidi (g)'].iloc[0] * self.food_list_amount[i] / 100).round(0)))
 
-            self.cal_count.append(df_def[df_def['Alimenti'] == self.food_list_name[i]].loc[:, 'KCal'] * self.food_list_amount[i] / 100)
-            self.carb_count.append(df_def[df_def['Alimenti'] == self.food_list_name[i]].loc[:, 'Carboidrati (g)'] * self.food_list_amount[i] / 100)
-            self.pro_count.append(df_def[df_def['Alimenti'] == self.food_list_name[i]].loc[:, 'Proteine (g)'] * self.food_list_amount[i] / 100)
-            self.lip_count.append(df_def[df_def['Alimenti'] == self.food_list_name[i]].loc[:, 'Lipidi (g)'] * self.food_list_amount[i] / 100)
+        for i in range(0, len(self.cal_count)):
+            self.calorie = self.calorie + self.cal_count[i]
+        for i in range(0, len(self.carb_count)):
+            self.carboidrati = self.carboidrati + self.carb_count[i]
+        for i in range(0, len(self.pro_count)):
+            self.proteine = self.proteine + self.pro_count[i]
+        for i in range(0, len(self.lip_count)):
+            self.lipidi = self.lipidi + self.lip_count[i]
 
-        """for i, val in range(0, len(self.cal_count)):
-            calorie = sum(self.cal_count[i])
-        for i in self.carb_count:
-            carboidrati = sum(self.carb_count[i])
-        for i in self.pro_count:
-            proteine = sum(self.pro_count[i])
-        for i in self.lip_count:
-            lipidi = sum(self.lip_count[i])"""
+        result = {
+            'KCal': [self.calorie],
+            'Carboidrati (g)': [self.carboidrati],
+            'Proteine (g)': [self.proteine],
+            'Lipidi (g)': [self.lipidi]
+            }
 
-        return self.cal_count #pd.DataFrame({
-                #'KCal': sum(self.cal_count),
-                #'Carboidrati (g)': sum(self.carb_count),
-                #'Proteine (g)': sum(self.pro_count),
-                #'Lipidi (g)': sum(self.lip_count)
-            #})
+        df_result = pd.DataFrame(result)
+
+        return df_result
+
+
